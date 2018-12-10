6960377a71d4b728bb4754c8cc0616def46773e4
Jakub Zegar <jakubzegar3@gmail.com>
2018-12-10 17:20:09 +0100

    kolosik :D

---
+++ logo.js
@@ -0,0 +1,7 @@
+let newdif = '<img src="ptaszek.png" alt=""/>';
+
+let dif = document.querySelector('.main');
+
+dif.innerHTML = newdif;
+
+
---
+++ script.js
@@ -0,0 +1,7 @@
+let button = document.querySelector('.button');
+
+button.addEventListener('click', function() {
+    var element = document.querySelector('.header')
+
+    element.style.display = 'none';
+});
